d4705053f5d7c8f9f6e7d0910a5828a88a9a3c76
Daniel Booysen <danielbooysenjr@gmail.com>
2023-02-10 19:33:56 +0200

    Add files via upload

---
+++ 100 Day Python Bootcamp - Intermediate/Day_17 - Quiz Project/custom_classes.py
@@ -0,0 +1,25 @@
+
+# Day 17 - Create Own Custom Classes
+    # A class is just an blueprint for an object to be used later on.
+
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "Daniel")
+user_2 = User("002", "Faith")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
